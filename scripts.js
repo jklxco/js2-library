5da71bdc8535304bf643fb4047f1b7be79885a86
jklxco <jack.a.coleman@hotmail.com>
2023-08-23 11:13:37 +0100

    Refactored Book constructor to class

--- scripts.js
+++ scripts.js
@@ -1,16 +1,32 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-}
+class Book {
+    constructor (title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 
-function addBookToLibrary(title, author, pages, read){
-    let newBook = new Book(title, author, pages, read)
-    myLibrary.push(newBook)
-    updateLibraryTable();
+    static toggleReadStatus(){
+        this.classList.toggle("read");
+        let index = this.parentNode.dataset.bookIndex;
+        (myLibrary[index].read == 'READ') ? myLibrary[index].read = 'NOT READ' :
+            myLibrary[index].read = 'READ';
+        this.innerText = myLibrary[index].read;
+    }
+
+    static removeBook(){
+        let index = this.closest('tr').dataset.bookIndex
+        myLibrary.splice(index, 1)
+        updateLibraryTable();
+    }
+
+    static addBookToLibrary(title, author, pages, read){
+        let newBook = new Book(title, author, pages, read)
+        myLibrary.push(newBook)
+        updateLibraryTable();
+    }
 }
 
 function updateLibraryTable(){
@@ -25,7 +41,7 @@ function updateLibraryTable(){
                 newCell.dataset.readButton = '';
                 newCell.classList.add('read-button');
                 (book[property] === 'READ') ? newCell.classList.add('read') : 0; 
-                newCell.addEventListener('click', toggleReadStatus);
+                newCell.addEventListener('click', Book.toggleReadStatus);
                 newRow.appendChild(newCell)
             } else {
                 let newCell = document.createElement('td');
@@ -34,12 +50,13 @@ function updateLibraryTable(){
             } 
         }
         let newCell = document.createElement('td');
-        let buttonRemove = document.createElement('button');
-        buttonRemove.innerText = 'REMOVE';
+        let buttonRemove = document.createElement('i');
+        buttonRemove.classList.add('fa-solid', 'fa-trash');
         buttonRemove.dataset.removeButton = '';
-        buttonRemove.addEventListener('click', removeBook);
+        buttonRemove.addEventListener('click', Book.removeBook);
         newCell.appendChild(buttonRemove);
         newRow.appendChild(newCell);
+
 
         tableBody.appendChild(newRow);       
     })    
@@ -53,24 +70,10 @@ function addFormEventListener(){
         let author = document.querySelector('input#author').value;
         let pages = document.querySelector('input#pages').value;
         let read = (document.querySelector('input[name=readStatus]:checked').value).toUpperCase();
-        addBookToLibrary(title, author, pages, read);
+        Book.addBookToLibrary(title, author, pages, read);
         document.querySelector('form').reset();
         newBookDialog.close();
     })
-}
-
-function toggleReadStatus(){
-    this.classList.toggle("read");
-    let index = this.parentNode.dataset.bookIndex;
-    (myLibrary[index].read == 'READ') ? myLibrary[index].read = 'NOT READ' :
-        myLibrary[index].read = 'READ';
-    this.innerText = myLibrary[index].read;
-}
-
-function removeBook(){
-    let index = this.closest('tr').dataset.bookIndex
-    myLibrary.splice(index, 1)
-    updateLibraryTable();
 }
 
 function showNewBookForm(){
@@ -83,7 +86,7 @@ function showNewBookForm(){
 const newBookButton = document.querySelector('[data-btn-new-book]');
 const newBookDialog = document.querySelector('[data-dialog-new-book');
 const tableBody = document.querySelector('tbody');
-addBookToLibrary('JS Generated Book', 'Author', 1337, 'NOT READ');
-addBookToLibrary('JS Generated Book 2', 'Author', 192, 'READ');
+Book.addBookToLibrary('JS Generated Book', 'Author', 1337, 'NOT READ');
+Book.addBookToLibrary('JS Generated Book 2', 'Author', 192, 'READ');
 addFormEventListener();
 showNewBookForm();
